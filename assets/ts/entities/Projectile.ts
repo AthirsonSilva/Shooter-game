69edb224d81a37f9f96ef80247a6e883afb79c03
Athirson <athirsonarceus@gmail.com>
2023-01-10 18:28:21 -0300

    Feat: TypeScript migration

---
+++ assets/ts/entities/Projectile.ts
@@ -0,0 +1,44 @@
+import { CONTEXT } from '../script'
+
+type ProjectileProps = {
+	x: number
+	y: number
+	radius: number
+	color: string
+	velocity: {
+		x: number
+		y: number
+	}
+}
+
+export class Projectile {
+	x: number
+	y: number
+	radius: number
+	color: string
+	velocity: {
+		x: number
+		y: number
+	}
+
+	constructor({ x, y, radius, color, velocity }: ProjectileProps) {
+		this.x = x
+		this.y = y
+		this.radius = radius
+		this.color = color
+		this.velocity = velocity
+	}
+
+	draw() {
+		CONTEXT.beginPath()
+		CONTEXT.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+		CONTEXT.fillStyle = this.color
+		CONTEXT.fill()
+	}
+
+	update() {
+		this.draw()
+		this.x = this.x + this.velocity.x
+		this.y = this.y + this.velocity.y
+	}
+}
